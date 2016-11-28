dc9870897f8f841e255d27efbd2bced434acc01c
Holden <alekseytyurinspb@gmail.com>
2016-11-28 15:09:05 +0300

    tested with gprof and valgrind

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # mmap_sorting
+
+*MMAP SORT* (test file sort.txt)
+Flat profile:
+
+Each sample counts as 0.01 seconds.
+| %time | cumulative seconds | self seconds | calls    | self s/call | total s/call | name               |
+|-------|--------------------|--------------|----------|-------------|--------------|--------------------|
+| 45.62 |               7.33 |         7.33 | 18673875 |        0.00 |         0.00 | comparator         |
+| 23.01 |              11.04 |         3.70 |   999999 |        0.00 |         0.00 | merge              |
+| 18.78 |              14.05 |         3.02 |      N/A |         N/A |          N/A | main               |
+|  6.03 |              15.03 |         0.97 |        1 |        0.97 |        12.01 | merge_sort_recurse |
+|  5.81 |              15.96 |         0.94 |      N/A |         N/A |          N/A | frame_dummy        |
+|  0.75 |              16.08 |         0.12 |        1 |        0.12 |        12.12 | merge_sort         |
+
+Call graph:
+
+
+
--- map.c
+++ map.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <time.h>
 #include "comparator.h"
 #include "merge_sort.h"
@@ -12,7 +13,8 @@ int main(int argc, char **argv)
 {
 	clock_t tic = clock();
 	if(argc != 3) exit(1);
-	int fd, strings_amount;
+	int fd; 
+	long int strings_amount;
 	struct stat file_info;
 	if((fd = open(argv[2], O_RDONLY, 0)) < 0)
 	 {
@@ -30,8 +32,8 @@ int main(int argc, char **argv)
 		printf("File's empty");
 		exit(1);
 	}
-	char *file = (char*)mmap(0, file_info.st_size , PROT_READ, MAP_PRIVATE, fd, 0) ;
-	printf("%ld\n", file_info.st_size);
+	char *file = (char*)mmap(0, file_info.st_size + 1 , PROT_READ, MAP_PRIVATE, fd, 0) ;
+	//printf("%ld\n", file_info.st_size);
 	if(file == MAP_FAILED)
 	{
 		fprintf(stdout, "Error, mmap file failed.\n");
@@ -39,14 +41,17 @@ int main(int argc, char **argv)
 	}
 	strings_amount = atoi(argv[1]);
 	char **strings = (char**)malloc(sizeof(char*) * strings_amount);
-	int i = 1, j = 0;
+	if(!strings) exit(1);
+	long int i = 1, j = 0;
 	strings[0] = &(file[0]);
-	while( (i!= strings_amount) && (j != file_info.st_size))
+	while(i != strings_amount) 
 	{
-		if((file[j++] == '\n') || (j == file_info.st_size -1)) 
+		if((file[j] == '\n'))
 		{
-			strings[i++] = &(file[j]);
+			strings[i++] = &(file[j+1]);
 		}
+		j++;
+		if(j == file_info.st_size - 1) break;
 	}
 	strings_amount = i;
 	merge_sort(strings, strings_amount);
@@ -64,6 +69,11 @@ int main(int argc, char **argv)
 	}
 
 	free(strings);
+	if((munmap(file, file_info.st_size + 1) == -1)) exit(1);
+	if(close(fd) == -1)
+	{
+		printf("Unable to close the file");
+	}
 	clock_t tac = clock();
 	printf("%f\n", (double)(tac-tic)/CLOCKS_PER_SEC);
 	return 0;
